2195a82942f1dcdb10f40f668efa5ac2a88aceb9
Umidjon <umidjongafforov175@gmail.com>
2023-11-08 14:09:07 +0500

    da

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://grand-shop-btis.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "https://grand-shop-btis.vercel.app/"
 
 
