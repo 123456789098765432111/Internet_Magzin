9f6708d4926fdddd2e0052d0728b5b03b80ae240
Umidjon <umidjongafforov175@gmail.com>
2023-11-09 18:17:10 +0500

    asd

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://cute-sunglasses-ray.cyclic.app/api/v2";
+export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "https://cute-sunglasses-ray.cyclic.app/"
+export const backend_url = "http://localhost:8000"
 
 
