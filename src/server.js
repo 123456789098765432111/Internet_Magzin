b28b51ab3d8ec07cea47fcbf8d39750940f1142f
Umidjon <umidjongafforov175@gmail.com>
2023-11-09 13:31:15 +0500

    1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://friendly-wasp-sweatpants.cyclic.app/api/v2";
+export const server = "https://good-puce-walrus-robe.cyclic.app/api/v2";
 
-export const backend_url = "https://friendly-wasp-sweatpants.cyclic.app/"
+export const backend_url = "https://good-puce-walrus-robe.cyclic.app/"
 
 
