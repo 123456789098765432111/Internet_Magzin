0c3ea9ab6558ad186e8eec38703e6afdb2fc8c50
Umidjon <umidjongafforov175@gmail.com>
2023-11-09 09:21:13 +0500

    s

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://drab-erin-meerkat-shoe.cyclic.app/api/v2";
+export const server = "https://friendly-wasp-sweatpants.cyclic.app/api/v2";
 
-export const backend_url = "https://drab-erin-meerkat-shoe.cyclic.app/"
+export const backend_url = "https://friendly-wasp-sweatpants.cyclic.app/"
 
 
