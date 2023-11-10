920f5c7ff069cc6cf9336721f53d8c9d85b8ca66
Umidjon <umidjongafforov175@gmail.com>
2023-11-10 22:10:09 +0500

    1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://backend-ten-wine-91.vercel.app/api/v2";
+export const server = "https://backend-frx6gtqzx-my-team-068bc198.vercel.app/api/v2";
 
-export const backend_url = "https://backend-ten-wine-91.vercel.app/"
+export const backend_url = "https://backend-frx6gtqzx-my-team-068bc198.vercel.app/"
 
 
