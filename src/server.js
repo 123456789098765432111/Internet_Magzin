7b627632f12a419c018c4361564bb53b691eef5c
Umidjon <umidjongafforov175@gmail.com>
2023-11-10 18:31:26 +0500

    asasasasaa

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://backend-44h75zafb-my-team-068bc198.vercel.app/api/v2";
+export const server = "https://backend-ten-wine-91.vercel.app/api/v2";
 
-export const backend_url = "https://backend-44h75zafb-my-team-068bc198.vercel.app/"
+export const backend_url = "https://backend-ten-wine-91.vercel.app/"
 
 
